19b5ac036f14aa5cc9a76cdcf75be0ed2d9663be
Patricia <ptcdiaz9@gmail.com>
2022-02-10 23:45:04 -0500

    add grasshopperCreateTheRooms.js

---
+++ 8th-Kyu/grasshopperCreateTheRooms.js
@@ -0,0 +1,17 @@
+// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+
+// Add rooms here
+var rooms = {
+    room1 : {
+      name: "FirstRoom",
+      description: "This is the First Room",
+      completed: "completed"},
+     room2 : {
+      name: "SecondRoom",
+      description: "This is the Second Room",
+      completed: "completed"},
+     room3 : {
+      name: "ThirdRoom",
+      description: "This is the Third Room",
+      completed: "completed"},
+   }
